75e1fe2ce517426b35470a17b1ef42a4547b8f24
Oldrich Lapka <lapka.oldrich@gmail.com>
2021-01-17 10:32:37 +0100

    - zdvojení generovaného čísla - nastavení unikátnosti gen. čísla
    - nastavení podmínek pro input hráče:
    
    1. unikátní čísla
    2. zamezení písmen
    3. zamezení '0' na prvním místě

--- PROJECT2_Bulls-Cows.py
+++ PROJECT2_Bulls-Cows.py
@@ -3,15 +3,22 @@ import random
 import time
 
 ODDELOVAC = '=' * 56
-list_rand = ['0', '1', '2', '3', '4',
+first_rand = ['1', '2', '3', '4',
+              '5', '6', '7', '8', '9']
+rest_rand = ['0', '1', '2', '3', '4',
              '5', '6', '7', '8', '9']
 
+genum = []
+a = True
 
-def generate_num() -> list:
-    return random.sample(list_rand, 4)
-
-generate_num()
-genum = generate_num()
+while a:
+    first = random.sample(first_rand, 1)
+    rest = random.sample(rest_rand, 3)
+    genum = first + rest
+    # print(genum)
+    if len(set(genum)) == 4 and genum[0:1] != 0:
+        genum = genum
+        a = False
 
 print("Hi there!",
       ODDELOVAC,
@@ -28,12 +35,19 @@ t0 = time.time()
 
 while not victory:
     guesses += 1
-    print(f'{genum=}')
-    NUMBER = list(input("Enter 4 digit number: "))
+    NUMBER = input("Enter 4 unique digits: ")
     if len(NUMBER) < 4 or len(NUMBER) > 4:
-        print('''Enter number of exactly FOUR digits
-Start again''')
+        print('Enter number of exactly FOUR unique digits')
+        break
+    elif NUMBER[:1] == '0':
+        print('The first digit must not be a zero.')
+        break
+    elif not NUMBER.isdecimal():
+        print('Enter only digits, not characters')
         print(ODDELOVAC)
+        break
+    elif len(set(NUMBER)) < 4:
+        print('Each of 4 digits must be unique')
         break
 
     bulls = 0
@@ -47,11 +61,11 @@ Start again''')
                       f' guessed the right number '
                       f'in {guesses} guesses!')
                 victory = True
-
             elif num == num1 and i != i1:
                 cows += 1
 
-    print(f'{bulls} bulls, {cows} cows,({guesses}. guess)')
+    print(str(bulls) + ' bulls,' + str(cows) + ' cows'
+          + f'{guesses}. guess'.rjust(36))
     print(ODDELOVAC)
 
 t1 = (time.time() - t0)
@@ -61,7 +75,7 @@ if victory and guesses <= 5:
 elif victory and 5 < guesses < 10:
     print("*** Very good MASTER !!! ***".center(56, '*'))
 elif victory and guesses > 10:
-    print("What a waste of time :D !!!".center(56))
+    print("You need to practice more  :) !!!".center(56))
 
 print(ODDELOVAC)
 print(f'Time elapsed:  {round(t1)} seconds.'.center(56))
